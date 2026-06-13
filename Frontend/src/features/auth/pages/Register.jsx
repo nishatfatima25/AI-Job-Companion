950c49ffdcfd9a3da1ad262c6175f90f45bc069f
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-06-13 23:40:45 +0530

    Create login and register UI, add auth services, context management and form styling

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Register = () => {
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      Register
+  <main>
+    <div className='form-container'>
+      <h1>Register</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <div className='input-group'>
+          <label htmlFor="email">Username</label>
+          <input type='text' id='text' name='text' placeholder='Enter username' />
+        </div>
+
+        <div className='input-group'>
+          <label htmlFor="email">Email</label>
+          <input type='email' id='email' name='email' placeholder='Enter email address' />
+        </div>
+
+        <div className='input-group'>
+          <label htmlFor="password">Password</label>
+          <input type='password' id='password' name='password' placeholder='Enter password' />
+        </div>
+
+        <button className='button primary-button'>Register</button>
+      </form>
+
+      <p>Already have an account? <Link to={"/login"}>Login</Link></p>
     </div>
+  </main>
   )
 }
 
